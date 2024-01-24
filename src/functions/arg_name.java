ef63fb1a4662aaf5935bd72ab13ec19c87ac7667
Usha shree <ushashree.gangaputra@gmail.com>
2024-01-24 08:10:52 -1200

    SECOND COMMIT

---
+++ src/functions/arg_name.java
@@ -0,0 +1,9 @@
+package src.functions;
+
+public class arg_name {
+
+    public static void main(String[] reference_name) //argument can be anythingits just reference name
+    {
+        System.out.println("THANKYOU UNIVERSE");
+    }
+}
