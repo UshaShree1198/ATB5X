c82ca09d7c94109820645b204934ae9673a63c29
Usha shree <ushashree.gangaputra@gmail.com>
2024-01-26 23:01:54 -1200

    26 jan

---
+++ src/basics/primeNumber.java
@@ -0,0 +1,23 @@
+package src.basics;
+
+import java.util.Scanner;
+
+public class primeNumber {
+    public static  void main(String[] args) {
+        //divisible by 1 and itself eg:5,17,19
+ int y= 17;
+        for (int i= 2; i<y ; i++)
+        {
+            if (y % i == 0) {
+                System.out.println(y + " is not prime number");
+                return;
+            }
+        }
+
+            System.out.println("prime");
+        }
+
+
+
+     }
+
