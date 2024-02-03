af52dc01dcd7a74af1352c66f21becb40bbf4417
Usha shree <ushashree.gangaputra@gmail.com>
2024-02-02 23:09:20 -1200

    feb 2

---
+++ src/strings/example2.java
@@ -0,0 +1,12 @@
+package src.strings;
+
+public class example2 {
+
+    String s1 = "usha";
+    StringBuilder sb = new StringBuilder("shree");
+   // String s3 = sb; // not possible
+    // String s3 = sb;  not possible
+     String s3 = sb.toString(); // we have to convert it to string
+
+    //string class is different and string builder class  and string bufferclass is different
+}
---
+++ src/strings/stringbuild.java
@@ -0,0 +1,11 @@
+package src.strings;
+
+public class stringbuild {
+    public static void main(String[] args) {
+        StringBuilder sd  = new StringBuilder("usha");
+        sd.append("shree");
+        System.out.println(sd);
+
+    }
+
+}
